3ed862629cddb4ce5f91ee3781b03d9a77cfaf95
daniellonsdale <danielllonsdale@gmail.com>
2025-03-06 00:58:06 +0900

    Add needed nodes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span.error");
+const postalCode = document.getElementById("postal-code");
+const postalCodeError = document.querySelector("#postal-code + span.error");
+const password = document.getElementById("password");
+const passwordError = document.querySelector("#password + span.error");
+const confirmPassword = document.getElementById("confirm-password");
+const confirmPasswordError = document.querySelector("#confirm-password + span.error");
